ce860185c877353c73e110b2b8aaa617aa9425b7
Panteleimon Kampouris <35311350+Ntelos@users.noreply.github.com>
2021-02-11 01:43:16 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
- 
+$(function(){
+	
+	var rowIdx = 1;
+
+	$("#addbutton").click(function(){
+		
+		$('#tbody').append(`<tr id="R${rowIdx}"> 
+								<td class="row-index text-center"> 
+									<p>Row ${rowIdx} insert name</p>
+								</td> 
+								<td class="text-center"> 
+									<p id="VR${rowIdx++}">0</p>
+								</td>
+								<td class="text-center"> 
+									<div class="container-fluid">
+										<div class="row">
+											<div class="col-6">
+												<button class="btn btn-success" type="button">+</button>
+												<button class="btn btn-danger" type="button">-</button>
+											</div>
+											<div class="col-6">
+												<button class="btn btn-warning" type="button">Erase</button>
+												<button class="btn btn-outline-danger" type="button">Remove</button> 
+											</div>
+										</div>
+									</div>
+								</td> 
+							</tr>`);
+	});
+	
+	//erase overlines name and disables votes
+
+});
